f108c30bdce651370373626a31a50c6d430d9e5c
swapnilshinde30 <swapnils@valueaddsofttech.com>
2023-06-06 09:42:14 +0530

    Update listgroup.jsx

--- src/routes/common/listgroup.jsx
+++ src/routes/common/listgroup.jsx
@@ -5,7 +5,7 @@ const ListGroup = (props) => {
     <ul className="list-group ml-10 mt-2 mr-4">
       {items.map((item) => (
         <li
-          className="rounded-lg  text-left mb-2 text-teal-500 transition duration-500 hover:bg-teal-100 hover:text-teal-700 focus:bg-neutral-100 focus:text-neutral-500 focus:ring-0 dark:hover:bg-neutral-600 dark:hover:text-neutral-200 dark:focus:bg-neutral-600 dark:focus:text-neutral-200"
+          className="rounded-lg  text-left py-2 text-neutral-500 transition duration-500 hover:bg-teal-100 hover:text-neutral-700 focus:bg-neutral-100 focus:text-neutral-500 focus:ring-0 dark:hover:bg-neutral-600 dark:hover:text-neutral-200 dark:focus:bg-neutral-600 dark:focus:text-neutral-200"
           key={item._id}
           style={{ cursor: "pointer" }}
           onClick={() => onSelectItem(item.name)}
